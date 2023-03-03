d697e926a0a872718a23db4690c31accfee7d814
MichalJurek <jurekmichal@o2.pl>
2023-03-03 19:52:07 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,9 @@
-const itemsRef = document.querySelectorAll('.item');
-console.log("Number of categories: ", itemsRef.length);
+const items = document.querySelectorAll(".item");
+console.log("Number of categories: ", items.length);
 
-
-  itemsRef.forEach(item => {
-    const itemsTitle = item.querySelector('.item h2');
-    console.log('Category: ', itemsTitle.textContent);
-    const itemsLength = item.querySelectorAll('.item > ul > li');
-    console.log('Elements: ', itemsLength.length);
+items.forEach(item => {
+  const categoryTitle = item.querySelector(".item h2");
+  console.log("Category:", categoryTitle.textContent)
+  const elementsCount = item.querySelectorAll("li")
+  console.log("Elements:", elementsCount.length)
 });
