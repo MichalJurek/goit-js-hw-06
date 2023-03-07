6e6f1dd02786d2031794d9351cfb672b24efc720
MichalJurek <jurekmichal@o2.pl>
2023-03-07 18:00:07 +0100

    zad 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const buttonColor = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const colorValue = document.querySelector(".color");
+
+buttonColor.addEventListener("click", (event) => {
+  const color = getRandomHexColor();
+   body.style.backgroundColor = color;
+   colorValue.textContent = color;
+ });
+
+
