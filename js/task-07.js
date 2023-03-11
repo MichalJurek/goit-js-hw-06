21eb0a5c1bdb5ee47c91b603b44ab949b27e96c8
MichalJurek <jurekmichal@o2.pl>
2023-03-11 18:28:18 +0100

    update task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const fontSizeChange = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
-
-fontSizeChange.addEventListener("change", (size) => {
-    text.style.fontSize = `${size.target.value}px`;
+// input działa płynniej niż change // 
+fontSizeChange.addEventListener("input", (event) => {
+    text.style.fontSize = fontSizeChange.value + "px"
 });
